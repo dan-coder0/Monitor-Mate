831ebd57820d8e9d5b3d732f99ffe3ac01a62cd4
dan-coder0 <dhanukarajapaksa1@gmail.com>
2025-10-21 09:08:27 +0530

    1 emoji replaced

--- src/components/common/RiskCategoryButton.js
+++ src/components/common/RiskCategoryButton.js
@@ -6,7 +6,7 @@ const RiskCategoryButton = ({ title, count, style, onPress }) => {
     const getIcon = () => {
         if (title.includes('High Risk')) return '🚨';
         if (title.includes('Medium Risk')) return '⚠️';
-        if (title.includes('Low Risk')) return '🟢 ';
+        if (title.includes('Low Risk')) return '🌟';
         if (title.includes('No Risk')) return '🛡️';
         return '📱';
     };
